bfbf2932da63bba20067866658e5d53182bfcc1c
HariRooshan <harirooshan007@gmail.com>
2025-07-25 09:39:19 +0530

    added auto increment to users table

--- sql/users.sql
+++ sql/users.sql
@@ -1,6 +1,6 @@
 -- Create the users table
 CREATE TABLE users (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY ,
     username VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50)
